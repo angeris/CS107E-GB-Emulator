463aa962c30e66eee10c58741807e2a5e193e7eb
Guillermo Angeris <guillean@stanford.edu>
2016-03-06 16:55:39 -0800

    Added some more types of values

--- cpu/MEM.c
+++ cpu/MEM.c
@@ -1,3 +1,11 @@
 #include "MEM.h"
 
-// TODO: Methods and crap.
+gb_short read8(gb_long addr) {
+    return _gb_rom[addr]
+}
+gb_long read16(gb_long addr) {
+
+}
+
+void write8(gb_long addr, gb_short val);
+void write16(gb_long addr, gb_long val);
--- cpu/MEM.h
+++ cpu/MEM.h
@@ -3,14 +3,39 @@
 
 #include <stdint.h>
 
-#define SIZE_KB     1024
-#define RAM_SIZE    8
-#define ROM_SIZE    8
+/* 
+ *         Layout of memory (from Pan Docs)
+ * ----------------------------------------------
+ * 0000-3FFF   16KB ROM Bank 00     (in cartridge, fixed at bank 00)
+ * 4000-7FFF   16KB ROM Bank 01..NN (in cartridge, switchable bank number)
+ * 8000-9FFF   8KB Video RAM (VRAM) (switchable bank 0-1 in CGB Mode)
+ * A000-BFFF   8KB External RAM     (in cartridge, switchable bank, if any)
+ * C000-CFFF   4KB Work RAM Bank 0 (WRAM)
+ * D000-DFFF   4KB Work RAM Bank 1 (WRAM)  (switchable bank 1-7 in CGB Mode)
+ * E000-FDFF   Same as C000-DDFF (ECHO)    (typically not used)
+ * FE00-FE9F   Sprite Attribute Table (OAM)
+ * FEA0-FEFF   Not Usable
+ * FF00-FF7F   I/O Ports
+ * FF80-FFFE   High RAM (HRAM)
+ * FFFF        Interrupt Enable Register
+ */
 
+#define SIZE_KB  8192
+
+// Units of work
 typedef uint16_t gb_long;
-typedef uint8_t gb_short;
+typedef int16_t  gb_long_s;
+typedef uint8_t  gb_short;
+typedef int8_t   gb_short_s;
 
-gb_short _gb_ram[RAM_SIZE*SIZE_KB];
-gb_short _gb_rom[ROM_SIZE*SIZE_KB]; //TODO: Needs to be changed if we're to be using banks.
+// Memory map.
+static gb_short _gb_rom[ROM_SIZE*SIZE_KB]; 
+
+gb_short read8(gb_long addr);
+gb_long read16(gb_long addr);
+
+void write8(gb_long addr, gb_short val);
+void write16(gb_long addr, gb_long val);
+
 
 #endif
