093f2750737026f08e6e9fd66da57cb15c1df8dd
Guillermo Angeris <guillean@stanford.edu>
2016-03-06 17:03:49 -0800

    More simple stuff

--- cpu/MEM.c
+++ cpu/MEM.c
@@ -1,11 +1,11 @@
 #include "MEM.h"
 
 gb_short read8(gb_long addr) {
-    return _gb_rom[addr]
+    return _gb_rom[addr];
 }
 gb_long read16(gb_long addr) {
-
+    return *(gb_long*)(&_gb_rom[addr]);
 }
 
-void write8(gb_long addr, gb_short val);
-void write16(gb_long addr, gb_long val);
+void write8(gb_long addr, gb_short val) { _gb_rom[addr] = val; }
+void write16(gb_long addr, gb_long val) { *(_gb_long)(&_gb_rom[addr]) = val; }
