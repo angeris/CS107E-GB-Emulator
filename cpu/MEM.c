cb364811c1f21eb27466e2bbe4550cfc35331d6a
Guillermo Angeris <guillean@stanford.edu>
2016-03-06 17:06:13 -0800

    Signed reading

--- cpu/MEM.c
+++ cpu/MEM.c
@@ -3,6 +3,10 @@
 gb_short read8(gb_long addr) {
     return _gb_rom[addr];
 }
+gb_short_s read8s(gb_long addr) {
+    return *(gb_short_s*)(&_gb_rom[addr]);
+}
+
 gb_long read16(gb_long addr) {
     return *(gb_long*)(&_gb_rom[addr]);
 }
