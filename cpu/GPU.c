dc9caeafdf435df7d5f562a72ce9a7d492018862
Guillermo Angeris <guillean@stanford.edu>
2016-03-09 01:44:25 -0800

    Finished up timings, still need to actually do scans and memory location stuff

---
+++ cpu/GPU.c
@@ -0,0 +1,53 @@
+#include "GPU.h"
+
+static unsigned _gpu_mode;
+static unsigned _gpu_clock;
+static unsigned _gpu_line;
+
+gb_short gpu_read(gb_long addr) {
+
+}
+
+void gpu_exec() {
+    _gpu_clock += 4;
+    switch(_gpu_mode) {
+    case MODE_HBLANK:
+        if(_gpu_clock >= 201) {
+            _gpu_clock = 0;
+            _gpu_line++;
+
+            if(_gpu_line>=143) {
+                _gpu_mode = MODE_VBLANK;
+                //TODO: draw everything into screen
+                return;
+            }
+            _gpu_mode = MODE_READOM;   
+        }
+        break;
+    case MODE_VBLANK:
+        if(_gpu_clock >= 4560) {
+            _gpu_mode = MODE_READOM;
+            _gpu_clock = 0;
+        }
+        break;
+    case MODE_READOM:
+        if(_gpu_clock >= 77) {
+            _gpu_mode = MODE_READAL;
+            _gpu_clock = 0;
+        }
+        break;
+    case MODE_READAL:
+        if(_gpu_clock >= 169) {
+            _gpu_mode = MODE_HBLANK;
+            _gpu_clock = 0;
+
+            gpu_writeline();
+        }
+        break;
+    }
+
+}
+
+void gpu_writeline() {
+    //TODO: A crapload of work
+}
--- cpu/GPU.h
+++ cpu/GPU.h
@@ -3,6 +3,22 @@
 
 #include "MEM.h"
 
+#define LCD_ENABLE      1<<7
+#define WIN_TILE_SELECT 1<<6
+#define WIN_DISP_ENABLE 1<<5
+#define WIN_BG_TILEDATA 1<<4
+#define BG_TILE_MAP_SEL 1<<3
+#define OBJ_SPRITE_SIZE 1<<2
+#define OBJ_SPRITE_ENAB 1<<1
+#define BG_DISPLAY_ENAB 1<<0
+
+#define MODE_HBLANK 0
+#define MODE_VBLANK 1
+#define MODE_READOM 2
+#define MODE_READAL 3
+
+extern gb_short vram[VRAM_SIZE];
+
 gb_short gpu_read(gb_long addr); 
 
 #endif
